808140dc091dbb3bb48f5c443662f5565690d537
Igor Rangel de Souza <163342979+Igorrangelsouza@users.noreply.github.com>
2026-04-07 14:54:57 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 DEBUG      = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', 'ongvlvlvercel10-production.up.railway.app'])
 
 CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=['http://localhost:3000'])
 CORS_ALLOW_CREDENTIALS = True
